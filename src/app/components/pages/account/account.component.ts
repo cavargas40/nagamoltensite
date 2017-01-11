549cd92f88965f97cb86b9eb1102b2b31ced6a9f
Carlos Vargas <cavargas40@gmail.com>
2017-01-11 14:16:40 -0500

    se arreglan unos errores del login para que se pueda llamar desde todas las paginas.

--- src/app/components/pages/account/account.component.ts
+++ src/app/components/pages/account/account.component.ts
@@ -14,7 +14,7 @@ export class AccountComponent implements OnInit {
   users: User[] = [];
 
   constructor(private userService: UserService) {
-    this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    //this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
   }
 
   ngOnInit() {
--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -2,19 +2,19 @@ import { Injectable } from '@angular/core';
 import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AlertService } from '../services';
 
-@Injectable()
-export class AuthGuard implements CanActivate {
-    constructor(private router:Router,
-                private alertService:AlertService) { }
+// @Injectable()
+// export class AuthGuard implements CanActivate {
+//     constructor(private router:Router,
+//                 private alertService:AlertService) { }
 
-    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
-        if (localStorage.getItem('currentUser')) {
-            //esta logueado, retorna true
-            return true;            
-        }
+//     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot){
+//         // if (localStorage.getItem('currentUser')) {
+//         //     //esta logueado, retorna true
+//         //     return true;            
+//         // }
 
-        //no esta logueado retorna a la pagina de login con la url de retorno
-        this.alertService.error("Debes estar logueado para ver esta pagina!", true);
-        this.router.navigate(['/login'], {queryParams:{ returnUrl: state.url }}); 
-    }
-}
+//         // //no esta logueado retorna a la pagina de login con la url de retorno
+//         // this.alertService.error("Debes estar logueado para ver esta pagina!", true);
+//         // this.router.navigate(['/login'], {queryParams:{ returnUrl: state.url }}); 
+//     }
+// }
